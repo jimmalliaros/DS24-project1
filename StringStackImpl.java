f57088627fdacdbe206a690601cf8c54b89c1d72
Jimmaliaros <jimmaliaros@gmail.com>
2024-11-30 10:55:17 +0200

    first changes in StringStackImpl.java

--- StringStackImpl.java
+++ StringStackImpl.java
@@ -1,3 +1,43 @@
+import java.util.NoSuchElementException;
+
 public class StringStackImpl implements StringStack{
+    private Node  top;
+    private int size=0;
+
+    public boolean isEmpty(){
+        return (size==0);
+    }
+
+    public void push(String item){
+        Node new_node=new Node(item);
+
+        if(top==null){
+            top=new_node;
+            size++;
+        }
+
+        else{
+            new_node.setNext(top);
+            top=new_node;
+            size++;
+        }
+    }
+
+    public String pop() throws NoSuchElementException {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        else {
+            String my_string=top.getData();
+            top=top.getNext();
+            size=size-1;
+            return my_string;
+
+        }
+    }
+
+
+
+
 
 }
