c766b358474d629f95eeee38c8915691c9023752
Jimmaliaros <jimmaliaros@gmail.com>
2024-12-01 14:10:17 +0200

    created DoubleQueueMain.java

---
+++ DoubleQueueMain.java
@@ -0,0 +1,15 @@
+public class DoubleQueueMain{
+    public static void main(String[] args){
+        DoubleQueue<Double> my_doublequeue=new DoubleQueueImpl<Double>();
+        my_doublequeue.put(0.0);
+        my_doublequeue.put(1.0);
+        my_doublequeue.put(2.0);
+        my_doublequeue.printQueue(System.out);
+
+        my_doublequeue.get();
+        my_doublequeue.printQueue(System.out);
+
+
+
+    }
+}
