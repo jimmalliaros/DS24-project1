2d2143e4d5c5b9cdd494288360933537bd73f4ce
Jimmaliaros <jimmaliaros@gmail.com>
2024-12-06 08:38:54 +0200

    small changes in stock.txt

--- NetProfit.java
+++ NetProfit.java
@@ -1,10 +1,21 @@
 import java.io.PrintStream;
 import java.util.NoSuchElementException;
-import java.util.Queue;
+import java.util.*;
 
 public class NetProfit{
     public static void main(String[] args){
-        DoubleQueue<Double> my_queue=new DoubleQueueImpl<Double>() ;
+        DoubleQueue<Double> prices_queue=new DoubleQueueImpl<Double>() ;
+        DoubleQueue<Double> amounts_queue=new DoubleQueueImpl<Double>() ;
+        Scanner filescanner=new Scanner("stock.txt");
 
+        while(filescanner.hasNextLine()){
+            String my_line=filescanner.nextLine();
+            Scanner linescanner =new Scanner(my_line);
+            while(linescanner.hasNext()){
+                String next=linescanner.next();
+                
+            }
+
+        }
     }
 }
